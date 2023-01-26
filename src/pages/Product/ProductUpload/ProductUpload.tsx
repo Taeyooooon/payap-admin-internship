46ce7b780debee11b20a55e42444ae364e066dc5
김도영 <go3152@gmail.com>
2023-01-26 16:29:08 +0900

    admin - product upload page - title Ui

--- src/pages/Product/ProductUpload/ProductUpload.tsx
+++ src/pages/Product/ProductUpload/ProductUpload.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const ProductUpload = () => {
-  return <div>liengift-admin product upload page</div>;
+  return (
+    <ContentsContainer>
+      <PageTitle>Upload product</PageTitle>
+      <RemarkMandatoryInput>* Mandatory field.</RemarkMandatoryInput>
+      test
+    </ContentsContainer>
+  );
 };
 
 export default ProductUpload;
+
+const ContentsContainer = styled.div`
+  padding: 30px 30px 30px 270px;
+`;
+
+const PageTitle = styled.div`
+  padding: 0px 0px 50px 0px;
+  font-size: 22px;
+  font-weight: 600;
+`;
+const RemarkMandatoryInput = styled.div`
+  padding: 0px 0px 30px 0px;
+  right: 30%;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 18px;
+`;
